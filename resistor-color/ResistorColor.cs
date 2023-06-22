3d741da5e45adbc925a2c5197851e7ef4a727b3c
anwar <anwarkp.qw@gmail.com>
2023-06-22 21:54:01 +0530

    Add solution to Resistor

---
+++ resistor-color/ResistorColor.cs
@@ -0,0 +1,32 @@
+using System;
+
+public static class ResistorColor
+{
+    public static int ColorCode(string color)
+    {
+        //color.ToUpper();
+        int code = color switch
+        {
+            "black" => 0,
+            "brown" => 1,
+            "red" => 2,
+            "orange" => 3,
+            "yellow" => 4,
+            "green" => 5,
+            "blue" => 6,
+            "violet" => 7,
+            "grey" => 8,
+            "white" => 9,
+            _ => throw new ArgumentOutOfRangeException(),
+
+        };
+        return code;
+    }
+
+    public static string[] Colors()
+    {
+        string[] colors = { "Black", "Brown", "Red", "Orange", "Yellow", "Green", "Blue", "Violet", "Grey", "White" };
+        return colors;
+    }
+    
+}
